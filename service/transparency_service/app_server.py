c8da38344163bd3408ce84450be690bdf06319a9
masha-maller-jfrog <masham@jfrog.com>
2025-05-09 02:18:36 +0300

    touchups

--- service/transparency_service/app_server.py
+++ service/transparency_service/app_server.py
@@ -1,33 +1,12 @@
 # transparency_service/app_server.py
-
-from contextlib import asynccontextmanager
-
-from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from transparency_service.api.commands.generate_response_command import GenerateResponseCommand
 from transparency_service.api.messages.generate_response_request import GenerateResponseRequest
 from transparency_service.api.messages.generated_response import GeneratedResponse
-from transparency_service.api.middleware.middleware import auth_middleware
+from transparency_service.api.auth.middleware import auth_middleware
 
-load_dotenv()
-
-# Add root route
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Lifespan startup: initialized parameters.")
-
-    yield
-
-    print("Lifespan shutdown: cleaning up.")
-
-transparency_app = FastAPI(lifespan=lifespan)
+transparency_app = FastAPI()
 transparency_app.middleware("http")(auth_middleware)
-
-
-@transparency_app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 
 @transparency_app.post("/transparency/generate_response")
 async def generate_response(payload: GenerateResponseRequest, response_model=GeneratedResponse):
