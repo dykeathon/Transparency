05fe62b97d840b6e9198d4936c7fd3af1fd6a9ec
Shery <sheryblichw@gmail.com>
2025-05-08 19:45:21 +0300

    Adding inital verify request skeleton and chaning readme

---
+++ service/transparency_service/verification/verify_request.py
@@ -0,0 +1,22 @@
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from flask import Flask, request, jsonify
+
+CLIENT_ID = "YOUR_CLIENT_ID.apps.googleusercontent.com"
+
+app = Flask(__name__)
+
+@app.route('/api/verify', methods=['POST'])
+def secure_data():
+    auth_header = request.headers.get('Authorization')
+    if not auth_header:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    token = auth_header.split("Bearer ")[-1]
+
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+        user_email = idinfo['email']
+        return jsonify({"message": f"valid user, {user_email}!"})
+    except ValueError:
+        return jsonify({"error": "Invalid token"}), 401
