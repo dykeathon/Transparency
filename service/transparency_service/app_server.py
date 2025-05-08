19cc57d2f57deaa9346e827345280fac92359156
Shery <sheryblichw@gmail.com>
2025-05-08 21:52:51 +0300

    Adding usage of env file and dotenv and verifiy

--- service/transparency_service/app_server.py
+++ service/transparency_service/app_server.py
@@ -1,8 +1,17 @@
+# transparency_service/app_server.py
+
 from fastapi import FastAPI
+from transparency_service.auth_routes import router as auth_router
+from dotenv import load_dotenv
+
+load_dotenv()
 
 transparency_app = FastAPI()
 
-
+# Add root route
 @transparency_app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# Include /api/verify route
+transparency_app.include_router(auth_router)
