6b7cb94a315f2f63612298e161f5f408e2a183b8
masha-maller-jfrog <masham@jfrog.com>
2025-05-09 09:04:15 +0300

    disabled auth validation for testing

--- service/transparency_service/app_server.py
+++ service/transparency_service/app_server.py
@@ -6,7 +6,7 @@ from transparency_service.api.messages.generated_response import GeneratedRespon
 from transparency_service.api.auth.middleware import auth_middleware
 
 transparency_app = FastAPI()
-transparency_app.middleware("http")(auth_middleware)
+# transparency_app.middleware("http")(auth_middleware)
 
 @transparency_app.post("/transparency/generate_response")
 async def generate_response(payload: GenerateResponseRequest, response_model=GeneratedResponse):
